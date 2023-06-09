53f3efa92e7b433f2f1a731f8bcc1a8f6b84c814
mark mwendwa <mwendwamark310@gmail.com>
2023-06-09 11:05:09 +0300

    add navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function Navbar() {
+  return (
+    <nav className="nav-link-container">
+      <li className="nav-link">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="nav-link">
+        <Link to="/login">Login</Link>
+      </li>
+      <li className="nav-link">
+        <Link to="/blogs">Blogs</Link>
+      </li>
+    </nav>
+  );
+}
+
+export default Navbar;
